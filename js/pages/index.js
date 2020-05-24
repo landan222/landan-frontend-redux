f993b5d063225936570f4fbb0031e89330afc70f
landan222 <landan222.tz@gmail.com>
2020-05-24 21:30:50 +0800

    SCSS

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,78 +1,5 @@
 $( window.document ).ready(function() {
-  var aUsers = [
-    {
-      user_id: 1,
-      name: 'Tom',
-      sex: '男',
-      age: 23,
-      location: '台北',
-      job: '学生',
-      tell: '0911-222-333'
-    },
-    {
-      user_id: 2,
-      name: 'Jam',
-      sex: '男',
-      age: 33,
-      location: '北京',
-      job: '教师',
-      tell: '0922-222-333'
-    },
-    {
-      user_id: 3,
-      name: 'Mary ken',
-      sex: '女',
-      age: 27,
-      location: '菲律宾',
-      job: '作业员',
-      tell: '0966-222-333'
-    },
-    {
-      user_id: 4,
-      name: 'Laplace Cheng',
-      sex: '男',
-      age: 44,
-      location: '未知',
-      job: '自由业',
-      tell: '0921-222-333'
-    },
-    {
-      user_id: 5,
-      name: 'Yam min',
-      sex: '男',
-      age: 44,
-      location: '未知',
-      job: '汽车销售员',
-      tell: '0921-222-333'
-    },
-    {
-      user_id: 6,
-      name: 'Tim xiao',
-      sex: '男',
-      age: 35,
-      location: '广东',
-      job: '保险业务员',
-      tell: '0951-222-333'
-    },
-    {
-      user_id: 7,
-      name: 'Joe Cheng',
-      sex: '男',
-      age: 22,
-      location: '天津',
-      job: '学生',
-      tell: '0971-222-333'
-    },
-    {
-      user_id: 8,
-      name: 'Ivy Cheng',
-      sex: '女',
-      age: 25,
-      location: '香港',
-      job: '待业',
-      tell: '0978-222-333'
-    }
-  ];
+
 
   $(window.document).on('click', 'section.number-wrapper #add', function (oEvent) {
     
@@ -109,6 +36,82 @@ $( window.document ).ready(function() {
   });
 
   $(window.document).on('click', 'section.users-wrapper #get', function (oEvent) {
+
+    var aUsers = [
+      {
+        user_id: 1,
+        name: 'Tom',
+        sex: '男',
+        age: 23,
+        location: '台北',
+        job: '学生',
+        tell: '0911-222-333'
+      },
+      {
+        user_id: 2,
+        name: 'Jam',
+        sex: '男',
+        age: 33,
+        location: '北京',
+        job: '教师',
+        tell: '0922-222-333'
+      },
+      {
+        user_id: 3,
+        name: 'Mary ken',
+        sex: '女',
+        age: 27,
+        location: '菲律宾',
+        job: '作业员',
+        tell: '0966-222-333'
+      },
+      {
+        user_id: 4,
+        name: 'Laplace Cheng',
+        sex: '男',
+        age: 44,
+        location: '未知',
+        job: '自由业',
+        tell: '0921-222-333'
+      },
+      {
+        user_id: 5,
+        name: 'Yam min',
+        sex: '男',
+        age: 44,
+        location: '未知',
+        job: '汽车销售员',
+        tell: '0921-222-333'
+      },
+      {
+        user_id: 6,
+        name: 'Tim xiao',
+        sex: '男',
+        age: 35,
+        location: '广东',
+        job: '保险业务员',
+        tell: '0951-222-333'
+      },
+      {
+        user_id: 7,
+        name: 'Joe Cheng',
+        sex: '男',
+        age: 22,
+        location: '天津',
+        job: '学生',
+        tell: '0971-222-333'
+      },
+      {
+        user_id: 8,
+        name: 'Ivy Cheng',
+        sex: '女',
+        age: 25,
+        location: '香港',
+        job: '待业',
+        tell: '0978-222-333'
+      }
+    ];
+
     // aUsers, 模拟 AJAX GET 后 服务器返回的 users 资料
     // 并更新到 store 里面， 再透过 store 统一渲染
     var oStore = {
